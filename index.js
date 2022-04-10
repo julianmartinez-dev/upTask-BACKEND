f3f24d738a7c25592ea42107cfee4762d7fc2774
Julian Martinez <julian.fmartinez@gmail.com>
2022-04-10 00:44:00 -0300

    add: configure cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import projectRoutes from './routes/projectRoutes.js';
@@ -9,9 +10,24 @@ const app = express();
 //Enable json reading from request body
 app.use(express.json());
 //Enable dontenv to read .env file
-dotenv.config()
+dotenv.config();
 //Connect to MongoDB
-connectDB()
+connectDB();
+
+//Enable CORS
+const whiteList = ['http://localhost:3000', 'https://localhost:3443'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.includes(origin)) {
+      //If origin is in the whitelist, allow it
+      callback(null, true);
+    } else {
+      //If origin is not in the whitelist, reject it
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+app.use(cors(corsOptions));
 
 //Routing
 app.use('/api/users', userRoutes);
@@ -21,5 +37,5 @@ app.use('/api/tasks', taskRoutes);
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
